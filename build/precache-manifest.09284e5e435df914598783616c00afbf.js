8b3ad8421ca4125a1618bc7ad174c228bbd1bb2c
Thucnv <nguyenvanthuc1912@gmail.com>
2020-07-06 14:48:12 +0700

    add file build

---
+++ build/precache-manifest.09284e5e435df914598783616c00afbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e24d220a9b5a797a549f54942bcf77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d27c408c204b3964ebe",
+    "url": "/static/css/main.ea4f0aa3.chunk.css"
+  },
+  {
+    "revision": "1db9ffc804ada845f233",
+    "url": "/static/js/2.e0f99ad6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0f99ad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d27c408c204b3964ebe",
+    "url": "/static/js/main.b0a7de32.chunk.js"
+  },
+  {
+    "revision": "3c6cec96fd74fc953344",
+    "url": "/static/js/runtime-main.a2543a3e.js"
+  }
+]);
